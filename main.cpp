74e4e50747c30e3b0892d4a10062c4c2ffd17be7
DESKTOP-VI73TAR\86135 <0815>
2021-12-26 17:40:37 +0800

    no message

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d %d\n", a, b);
     return 0;
 }
