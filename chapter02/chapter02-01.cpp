650ed05a20d48c013ed8d0d231be566c38b84a37
DESKTOP-VI73TAR\86135 <0815>
2021-12-26 17:40:44 +0800

    no message

--- chapter02/chapter02-01.cpp
+++ chapter02/chapter02-01.cpp
@@ -2,3 +2,12 @@
 // Created by 86135 on 2021/12/26.
 //
 
+#include <cstdio>
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    printf("%d %d\n", a, b);
+    return 0;
+}
--- chapter02/myfirst.cpp
+++ chapter02/myfirst.cpp
@@ -2,3 +2,12 @@
 // Created by 86135 on 2021/12/18.
 //
 
+#include <iostream>
+//#include <math.h>
+int main()
+{
+    printf("hello world\n");
+    std::cout << "hello world";
+    return 0;
+}
+
