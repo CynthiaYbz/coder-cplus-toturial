e06605541406dd4da45db527d9c18db6b5ba9fee
DESKTOP-VI73TAR\86135 <0815>
2022-01-02 14:53:55 +0800

    no message

--- chapter02/chapter02-01.cpp
+++ chapter02/chapter02-01.cpp
@@ -1,13 +1,18 @@
 //
-// Created by 86135 on 2021/12/26.
+// 题目： 显示姓名和地址
 //
-
-#include <cstdio>
+// 问题：
+// - 中文乱码： https://www.zhihu.com/question/421326588
+#include <iostream>
 
 int main()
 {
-    int a = 10;
-    int b = 20;
-    printf("%d %d\n", a, b);
+    using namespace std;
+    system("chcp 65001");
+//    setlocale(LC_ALL, "zh-CN");
+    cout << "My name is coder\n";
+    cout << "My address is beijing\n";
+    cout << "我是小码匠";
+    cout << "我家住在北京市";
     return 0;
 }
