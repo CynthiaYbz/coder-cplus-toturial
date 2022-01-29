9caa77cae14a75e7ff03fa5f43cff78b694d73ee
DESKTOP-VI73TAR\86135 <0815>
2022-01-29 15:56:12 +0800

    no message

--- chapter04/chapter04-03.cpp
+++ chapter04/chapter04-03.cpp
@@ -1,8 +1,3 @@
-// 小码匠和老码农：一对爱逗比、爱编程、爱分享父女，关注小码匠和老码农，一起分享知识，一起精进技术
-// - 微信公众号： 搜索【小码匠和老码农】
-// - 小码匠和老码农： http://www.coder-oldgeek.com/
-// - 知乎： https://www.zhihu.com/people/coder-oldgeek
-// - B站： https://space.bilibili.com/1511015660
 //
 // 读书： C++ Primer Plus（第6版）中文版
 // 编程练习：4.13.3
