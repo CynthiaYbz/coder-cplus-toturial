379e92a671d54ac31a58b9cabfa08ebdf3be2732
DESKTOP-VI73TAR\86135 <0815>
2022-01-29 11:37:57 +0800

    初始化头部内容

--- chapter02/chapter02-01.cpp
+++ chapter02/chapter02-01.cpp
@@ -1,21 +1,10 @@
+// 小码匠和老码农：一对爱逗比、爱编程、爱分享父女，关注小码匠和老码农，一起分享知识，一起精进技术
+// - 微信公众号： 搜索【小码匠和老码农】
+// - 小码匠和老码农： http://www.coder-oldgeek.com/
+// - 知乎： https://www.zhihu.com/people/coder-oldgeek
+// - B站： https://space.bilibili.com/1511015660
 //
-// 题目： 显示姓名和地址
-//
-// 问题：
-// - 中文乱码： https://www.zhihu.com/question/421326588
-#include <iostream>
-
-int main()
-{
-    system("chcp 65001");
-    std::cout << "My name is Cynthia.";
-    std::cout << "我叫靳灏芊，我家住北苑家园";
-    return 0;
-}
-
-
-
-=======
+// 读书： C++ Primer Plus（第6版）中文版
 // 编程练习：2.7.1
 //
 // 参考资料列表：
@@ -34,4 +23,3 @@ int main()
 //    cout << "我家住在北京市";
     return 0;
 }
->>>>>>> feature/feature_20220101
